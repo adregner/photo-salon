40ea17be453d402231515e95d958814c9d89c49f
Andrew Regner <andrew@aregner.com>
2026-05-25 20:25:50 -0400

    Fetch other Windows build dependencies from s3

--- build-windows.sh
+++ build-windows.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
+set -euo pipefail
+
+"$(dirname "$0")/fetch-windows-deps.sh"
+
 cmake -B _build_win --toolchain cmake/toolchains/windows-x86_64-clang-cl.cmake -DCMAKE_BUILD_TYPE=Release
 cmake --build _build_win
